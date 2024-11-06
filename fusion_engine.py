b4c218fa7d140f3b790bd4e47a2694177c04bd6b
Zizeo <elio.genson@gmail.com>
2024-11-06 14:43:19 +0100

    modified fusion_engine

--- fusion_engine.py
+++ fusion_engine.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
 
 from ivy.std_api import *
-IvyInit("fusion_engine")
+from typing import Any
+
+def info(fmt: str, *arg: Any) -> None:
+    print(fmt % arg)
+
+def on_connection_change(agent: IvyClient, event: int) -> None:
+    if event == IvyApplicationDisconnected:
+        info('Ivy application %r has disconnected', agent)
+    else:
+        info('Ivy application %r has connected', agent)
+    apps = IvyGetApplicationList()
+    info(
+        'Ivy applications currently on the bus (count: %i): %s',
+        len(apps),
+        ','.join(apps),
+    )
+
+
+IvyInit("fusion_engine","hi",0,on_connection_change)
 IvyStart()
 forme=""
+
+
+
 
 
 def dollarN_callback(agent, arg):
@@ -19,4 +40,4 @@ def sra5_callback(agent, arg):
 
 IvyBindMsg(dollarN_callback, "^dollarN: (.*)")
 IvyBindMsg(sra5_callback, "^sra5: (.*)")
-IvyBindMsg(sra5_callback, ".*")
+IvyBindMsg(sra5_callback, "(.*)")
