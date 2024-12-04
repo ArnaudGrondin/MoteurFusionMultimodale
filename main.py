a51b96397b670638271ffeb8e5adac74df819499
ArnaudG <grondin828@gmail.com>
2024-12-04 10:19:25 +0100

    "Ajout dessin de rectangle "
    ajout de la possibilité de dessiner un rectangle à la voix avec des coordonnées et une taille fixe

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import simple
 import fusion_engine
 import sys
 import pygame
+from pygame import Rect,Surface
 import os
    
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -24,28 +25,55 @@ import os
 #         self.quitButton = tk.Button(self, text='Quit',command=self.quit)            
 #         self.quitButton.grid()    
 
+def dessiner_forme(fenetre,forme,coord=(210,180), couleur =(255,0,0) ):
+    rect = Rect(coord,(180,200))
+
+    match forme:
+        case 'RECTANGLE':
+            pygame.draw.rect(fenetre,couleur,rect)
+        case 'CIRCLE':
+            pass
+        case 'DIAMOND':
+            pass
+        case 'TRIANGLE':
+            pass
+        case _:
+            pass
+
 def main():
     pygame.init()
+    liste_cmd = list()
     fenetre = pygame.display.set_mode((800, 600))
     running = True
     clock = pygame.time.Clock()
     motor = fusion_engine.FusionMotor()
     font = pygame.font.SysFont(None, 32)
+    fenetre.fill("white")
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        action = ""
+        if len(motor.sra5_token) > 3 : #todo rajouter une condition sur le taux de confiance
+            score = float( motor.sra5_dict['Confidence'].replace(',','.'))
+            if score > 0.6:
 
-        fenetre.fill("white")
-        #texte = str( motor.forme)
-        print(motor.sra5_callback)
+                #print(float(score))
+                print(f"{motor.sra5_dict}")
+                liste_cmd.append(motor.sra5_dict) # on veut un historique des commandes
+                action = motor.sra5_dict['action']
+                forme = motor.sra5_dict['form']
+                dessiner_forme(fenetre,forme)
+            #print("yes " + text )
+
        # print("Token " + str(len(motor.sra5_token)) )
-        print("String " + motor.sra5_string)
-        text_surface = font.render("texte", True, (255, 255, 255), (0, 0, 0))
+        #print("String " + motor.sra5_string)
+        text_surface = font.render(action, True, (255, 255, 255), (0, 0, 0))
         # rend les informations graphiques à l'écran 
         fenetre.blit(text_surface, (20, 20))
         pygame.display.flip()
-        dt = clock.tick(60) / 1000 #limite les fps a 60
+        dt = clock.tick(10) / 1000 #limite les fps a 60
         
     # Gestion des événements
     # ...
